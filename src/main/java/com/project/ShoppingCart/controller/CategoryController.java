e044d1830fd70e2823758c941bc2b74d6f958551
SwarnenduGanguli25 <swarnenduganguli@gmail.com>
2022-03-04 21:03:48 +0530

    Update Category API created

--- src/main/java/com/project/ShoppingCart/controller/CategoryController.java
+++ src/main/java/com/project/ShoppingCart/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package com.project.ShoppingCart.controller;
 
+import com.project.ShoppingCart.common.ApiResponse;
 import com.project.ShoppingCart.model.Category;
 import com.project.ShoppingCart.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,13 +18,22 @@ public class CategoryController {
     CategoryService categoryService;
 
     @PostMapping("/add")
-    public String createCategory(@RequestBody Category category){
+    public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
-        return "success";
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "New Category Created"), HttpStatus.CREATED);
     }
 
     @GetMapping("/getAll")
     public List<Category> getAllCategory(){
         return categoryService.getAllCategory();
+    }
+
+    @PutMapping("/update/{categoryId}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryId") int categoryId, @RequestBody Category category){
+        if(!categoryService.findById(categoryId)){
+            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "Category with this id does not exist"), HttpStatus.CONFLICT);
+        }
+        categoryService.updateCategory(categoryId, category);
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Category has been updated"), HttpStatus.OK);
     }
 }
--- src/main/java/com/project/ShoppingCart/service/CategoryService.java
+++ src/main/java/com/project/ShoppingCart/service/CategoryService.java
@@ -14,11 +14,22 @@ public class CategoryService {
     CategoryRepo categoryRepo;
 
     public void createCategory(Category category){
-
         categoryRepo.save(category);
     }
 
     public List<Category> getAllCategory(){
         return categoryRepo.findAll();
+    }
+
+    public void updateCategory(int categoryId, Category updateCategory) {
+        Category category = categoryRepo.getById(categoryId);
+        category.setCategoryName(updateCategory.getCategoryName());
+        category.setDescription(updateCategory.getDescription());
+        category.setImageUrl(updateCategory.getImageUrl());
+        categoryRepo.save(category);
+    }
+
+    public boolean findById(int categoryId) {
+        return categoryRepo.findById(categoryId).isPresent();
     }
 }
