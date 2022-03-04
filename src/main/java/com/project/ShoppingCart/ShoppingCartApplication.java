6adbdad36fdb8ba0d04a336fc63a6466d01fd77b
SwarnenduGanguli25 <swarnenduganguli@gmail.com>
2022-03-04 19:09:37 +0530

    First commit

--- src/main/java/com/project/ShoppingCart/ShoppingCartApplication.java
+++ src/main/java/com/project/ShoppingCart/ShoppingCartApplication.java
@@ -8,6 +8,8 @@ public class ShoppingCartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartApplication.class, args);
+
+		System.out.println("Testing Git..");
 	}
 
 }
